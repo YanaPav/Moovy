87bd54f71c4c52d974d10fd4e95116f166e4b953
Yana Pavlyk <pavlikyg@gmail.com>
2022-10-31 21:18:15 +0200

    Finish updates

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,16 @@
 import { lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import CssBaseline from '@mui/material/CssBaseline';
-import { SearchPage } from '../Pages/SearchPage/SearchPage';
-import { NavBar } from '../components/NavBar/NavBar';
+import { SearchPage } from 'Pages/SearchPage/SearchPage';
+import { NavBar } from 'components/NavBar/NavBar';
 
 const RatedMoviesPage = lazy(() =>
-  import('../Pages/RatedMoviesPage/RatedMoviesPage')
+  import('Pages/RatedMoviesPage/RatedMoviesPage')
 );
 const MovieDetailsPage = lazy(() =>
-  import('../Pages/MovieDetailsPage/MovieDetailsPage')
+  import('Pages/MovieDetailsPage/MovieDetailsPage')
 );
-const PageNotFound = lazy(() => import('../Pages/PageNotFound/PageNotFound'));
+const PageNotFound = lazy(() => import('Pages/PageNotFound/PageNotFound'));
 
 export const App = () => {
   return (
